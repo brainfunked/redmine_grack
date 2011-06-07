494a535b1a5f661b7f9aac865b0b9a6db009f880
Mrugesh Karnik <mrugesh@windu.geodesic.net>
2011-06-07 12:46:53 +0530

    Overrode find_optional_project to find a project by name, rather than just the numeric id

--- app/controllers/grack_controller.rb
+++ app/controllers/grack_controller.rb
@@ -10,4 +10,16 @@ class GrackController < ApplicationController
   def rw
     render :text => "OK"
   end
+
+  private
+
+  # Overriding the main find_optional_project, which needs
+  # a numeric id, but we have a textual id.
+  def find_optional_project
+    @project = (Project.find(params[:project_id]) || Project.find_by_name(params[:project_id].to_s)) unless params[:project_id].blank?
+    allowed = User.current.allowed_to?({:controller => params[:controller], :action => params[:action]}, @project, :global => true)
+    allowed ? true : deny_access
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
 end
